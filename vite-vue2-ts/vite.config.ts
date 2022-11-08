6bd6aa7e337994ee5127712b6d2ea845b1a0345e
GitLqr <960432662@qq.com>
2022-11-08 15:24:35 +0800

    refact: 配置 `@vitejs/plugin-legacy`

--- vite-vue2-ts/vite.config.ts
+++ vite-vue2-ts/vite.config.ts
@@ -1,5 +1,31 @@
 import { createVuePlugin } from "vite-plugin-vue2";
+import legacy from "@vitejs/plugin-legacy";
 
 export default {
-  plugins: [createVuePlugin()],
+  plugins: [
+    createVuePlugin(),
+    legacy({
+      targets: ["android 4", "ios 8", "chrome 52", "ie 11"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+      renderLegacyChunks: true,
+      polyfills: [
+        "es.symbol",
+        "es.array.filter",
+        "es.promise",
+        "es.promise.finally",
+        "es/map",
+        "es/set",
+        "es.array.for-each",
+        "es.object.define-properties",
+        "es.object.define-property",
+        "es.object.get-own-property-descriptor",
+        "es.object.get-own-property-descriptors",
+        "es.object.keys",
+        "es.object.to-string",
+        "web.dom-collections.for-each",
+        "esnext.global-this",
+        "esnext.string.match-all",
+      ],
+    }),
+  ],
 };
