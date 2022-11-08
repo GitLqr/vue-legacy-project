a57b4b6056eeee0b927b253b159b29ea1ede044c
GitLqr <960432662@qq.com>
2022-11-08 14:35:36 +0800

    refact: webpack-vue2-ts 配置babel兼容低版本浏览器

--- webpack-vue2-ts/babel.config.js
+++ webpack-vue2-ts/babel.config.js
@@ -1,5 +1,21 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+    "@vue/cli-plugin-babel/preset",
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "entry",
+        corejs: 3,
+        targets: {
+          ios: "8",
+          android: "4",
+          chrome: "58",
+        },
+      },
+    ],
+  ],
+  plugins: [
+    "@babel/plugin-transform-arrow-functions",
+    "@babel/plugin-proposal-optional-chaining",
+  ],
+};
